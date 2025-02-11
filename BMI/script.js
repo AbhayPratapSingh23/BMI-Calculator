047d36a0203f6ab05a2d662012e1ff304f5bd760
Abhay Pratap Singh <hvsinghe@gmail.com>
2025-02-11 11:17:08 -0800

    script.js

--- BMI/script.js
+++ BMI/script.js
@@ -3,6 +3,10 @@ function calculateBMI() {
     let weight = parseFloat(document.getElementById("weight").value);
     let height = parseFloat(document.getElementById("height").value);
 
+    console.log("Gender:", gender);
+    console.log("Weight:", weight);
+    console.log("Height:", height);
+
     if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
         alert("Please enter valid weight and height!");
         return;
@@ -12,18 +16,18 @@ function calculateBMI() {
     document.getElementById("bmi-value").textContent = bmi;
 
     let category = "";
-    let rotation = 90; // Default to normal weight
+    let rotation = 90;
 
     if (gender === "male") {
         if (bmi < 18.5) {
             category = "Underweight";
-            rotation = 45;
+            rotation = 50;
         } else if (bmi >= 18.5 && bmi < 25) {
             category = "Normal Weight";
             rotation = 90;
         } else if (bmi >= 25 && bmi < 30) {
             category = "Overweight";
-            rotation = 135;
+            rotation = 130;
         } else {
             category = "Obese";
             rotation = 170;
@@ -31,13 +35,13 @@ function calculateBMI() {
     } else {
         if (bmi < 18.0) {
             category = "Underweight";
-            rotation = 45;
+            rotation = 50;
         } else if (bmi >= 18.0 && bmi < 24) {
             category = "Normal Weight";
             rotation = 90;
         } else if (bmi >= 24 && bmi < 29) {
             category = "Overweight";
-            rotation = 135;
+            rotation = 130;
         } else {
             category = "Obese";
             rotation = 170;
@@ -45,5 +49,7 @@ function calculateBMI() {
     }
 
     document.getElementById("bmi-category").textContent = category;
-    document.getElementById("needle").style.transform =' rotate(${rotation}deg)';
+    document.getElementById("needle").style.transform =`rotate(${rotation}deg)`;
+
+    console.log("BMI:", bmi, "Category:", category);
 }
